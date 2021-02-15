9e0e58ed64f90644e21d9efa1d750447f0ebfa26
AlexeyLK <57331312+AlexeyLK@users.noreply.github.com>
2021-02-15 20:01:51 +0200

    Add files via upload

---
+++ Task3/main.c
@@ -0,0 +1,61 @@
+#include <thread>
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+ 
+int main(int argc, char *argv[])
+//argv[1] - dir
+//argv[2] - file protocol
+{
+    std::thread th_1;
+    std::thread th_2;
+    //th_1.join();
+    //th_1.join();
+    if ((argc<2) || (argc>3))
+    {
+        printf("2 or 3 parameters required\n");
+        return -1;
+    }
+    else
+    {
+        int fd;
+        if (!(fd=open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0666)))
+        {
+            printf("Can\'t create file\n");
+            return -1;
+        }
+        else
+        {
+            DIR *dir;
+            if ((dir=opendir(argv[1]))==NULL)
+            {
+                printf("Can\'t open folder\n");
+                return -1;
+            }
+            else
+            {
+                struct dirent *f_cur;
+                while ((f_cur=readdir(dir))!=NULL)
+                {
+                    if (argc==3)
+                    {
+                        int i=0;
+                        for (i=0; i<strlen(f_cur->d_name); ++i) write(fd, &f_cur->d_name[i], 1);
+                        write(fd, "\n", 1);
+                    }
+                    else printf("%s\n", f_cur->d_name);
+                }
+                printf("Done...\n");
+                close(fd);
+                closedir(dir);  
+                return 0;
+            }   
+        }
+    }
+}
